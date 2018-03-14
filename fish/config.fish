c5b2d1543f51ec88d958db7133e4e6870559c4b2
Rich Leland <rich.leland@avaaz.org>
2018-03-14 16:17:06 -0400

    pull in custom functions

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,20 @@
+###############################################################################
+# set some variables
+###############################################################################
 set -x EDITOR vim
 set -x GIT_EDITOR vim
 set -x PATH $HOME/src/oss/gitplus/commands /usr/local/opt/libxml2/bin /usr/local/opt/python/libexec/bin $PATH
 set -x VIRTUALENV_PYTHON python3
 
+###############################################################################
+# activate virtualfish
+###############################################################################
 eval (python3 -m virtualfish)
+
 
 ###############################################################################
 # pull in my custom functions
 ###############################################################################
-source custom/*.fish
+for item in $HOME/my/dotfiles/fish/custom/*.fish
+    source $item
+end
