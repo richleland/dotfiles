206eadde579d621f898ab6503e897d6546d29023
Rich Leland <rich.leland@avaaz.org>
2018-08-24 16:52:15 -0400

    latest changes

--- fish/config.fish
+++ fish/config.fish
@@ -3,13 +3,22 @@
 ###############################################################################
 set -x EDITOR vim
 set -x GIT_EDITOR vim
-set -x PATH $HOME/src/oss/gitplus/commands /usr/local/opt/libxml2/bin /usr/local/opt/python/libexec/bin $PATH
+set -x PATH $HOME/.composer/vendor/bin ./node_modules/.bin $HOME/src/oss/gitplus/commands /usr/local/opt/libxml2/bin /usr/local/opt/python/libexec/bin /usr/local/sbin $PATH
 set -x VIRTUALENV_PYTHON python3
+set -x PROJECT_HOME $HOME/src/projects
+set -x LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN y
+
+
+###############################################################################
+# aliases
+###############################################################################
+alias vi="mvim -v"
+alias vim="mvim -v"
 
 ###############################################################################
 # activate virtualfish
 ###############################################################################
-eval (python3 -m virtualfish)
+eval (python3 -m virtualfish auto_activation compat_aliases)
 
 
 ###############################################################################
