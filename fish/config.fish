876c665b6fd362bfa3b3c826823e6e7f82254fcd
Rich Leland <rich.leland@avaaz.org>
2019-10-04 10:56:19 -0400

    update paths for pipenv and add rbenv

--- fish/config.fish
+++ fish/config.fish
@@ -3,10 +3,11 @@
 ###############################################################################
 set -x EDITOR "mvim -v"
 set -x GIT_EDITOR "mvim -v"
-set -x PATH $HOME/.cargo/bin $HOME/.composer/vendor/bin /usr/local/opt/php@7.2/bin ./node_modules/.bin $HOME/src/oss/gitplus/commands /usr/local/opt/libxml2/bin /usr/local/opt/python/libexec/bin /usr/local/sbin $PATH
+set -x PATH $HOME/.poetry/bin $HOME/.cargo/bin $HOME/.composer/vendor/bin /usr/local/opt/php@7.2/bin ./node_modules/.bin $HOME/src/oss/gitplus/commands /usr/local/opt/libxml2/bin /usr/local/opt/python/libexec/bin /usr/local/sbin $PATH
 set -x VIRTUALENV_PYTHON python3
 set -x PROJECT_HOME $HOME/src/projects
 set -x LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN y
+set -x PIPENV_VENV_IN_PROJECT y
 
 
 ###############################################################################
@@ -15,10 +16,17 @@ set -x LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN y
 alias vi="mvim -v"
 alias vim="mvim -v"
 
+
 ###############################################################################
 # activate virtualfish
 ###############################################################################
 eval (python3 -m virtualfish auto_activation compat_aliases)
+
+
+###############################################################################
+# activate rbenv
+###############################################################################
+status --is-interactive; and source (rbenv init -|psub)
 
 
 ###############################################################################
