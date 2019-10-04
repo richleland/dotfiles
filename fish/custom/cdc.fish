079c3b6550038daedd0807328ec9eb1d10a8f5dc
Rich Leland <rich.leland@avaaz.org>
2019-10-04 13:15:04 -0400

    update cd commands to reflect new machine

--- fish/custom/cdc.fish
+++ fish/custom/cdc.fish
@@ -2,5 +2,5 @@ function cdc --description 'move to code folder' --argument subfolder
     # default to no sub-folder
     set -q subfolder[1]
     or set subfolder "."
-    cd /Volumes/Laserbeak/code/$subfolder
+    cd $HOME/code/$subfolder
 end
--- fish/custom/cdp.fish
+++ fish/custom/cdp.fish
@@ -2,5 +2,5 @@ function cdp --description 'move to projects folder' --argument subfolder
     # default to no sub-folder
     set -q subfolder[1]
     or set subfolder "."
-    cd /Volumes/Laserbeak/code/projects/$subfolder
+    cd $HOME/code/projects/$subfolder
 end
