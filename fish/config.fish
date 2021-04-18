10d845ee7268296a928a2683e85cd943df978ccc
Rich Leland <rich@richleland.com>
2021-04-17 22:38:25 -0400

    unset EDITOR value

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,6 @@
 ###############################################################################
 # set some variables
 ###############################################################################
-set -x EDITOR "mvim -v"
-set -x GIT_EDITOR "mvim -v"
 set -x PATH $HOME/.poetry/bin $HOME/.cargo/bin $HOME/.composer/vendor/bin /usr/local/opt/php@7.2/bin $HOME/.nodenv/bin ./node_modules/.bin $HOME/src/oss/gitplus/commands /usr/local/opt/libxml2/bin /usr/local/opt/python/libexec/bin /usr/local/sbin $PATH
 set -x VIRTUALENV_PYTHON python3
 set -x PROJECT_HOME $HOME/src/projects
